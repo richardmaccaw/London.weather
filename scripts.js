1426b22b603ddd8b1d3e4f77e529c31d83f9d165
richardmaccaw <ricci6@gmail.com>
2018-09-13 10:30:38 +0100

    Updated icons and added repeat to load page faster

--- scripts.js
+++ scripts.js
@@ -8,29 +8,35 @@ const getWeather = async () => {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?&units=metric&lat=51&lon=-0.13&&APPID=0929cfdb09431c5749641254e1e34af9`)
   let data = await response.json()
   
-  // show the response
+  // show the weather response
   let weatherContent = document.createTextNode(data.weather[0].main)
   weatherDiv.appendChild(weatherContent)
 
-  // show the image
+  //show the right weather background
   const getImage = function () {
-    let backgroundImage = document.querySelector('.myContainer')
-    if (data.weather[0].main == "Rain") {
-      backgroundImage.style.backgroundImage = "url(img/rain.png)"
-    } else if (data.weather[0].main == "Sun") {
-      backgroundImage.style.backgroundImage = "url(img/sun.png)"
+    
+    let background = document.querySelector('.background')
+    if (data.weather[0].main == "Clear") {
+      background.style.backgroundImage = "url(img/sun_icon.png)"
+      background.style.backgroundColor = '#F8E71C'
+    } else if (data.weather[0].main == "Rain") {
+      background.style.backgroundImage = "url(img/rain_icon.png)"
+      background.style.backgroundColor = '#4A90E2'
     } else if (data.weather[0].main == "Clouds") {
-      backgroundImage.style.backgroundImage = "url(img/clouds.png)"
+      background.style.backgroundImage = "url(img/cloud_icon.png)"
+      background.style.backgroundColor = '#676767'
+    } else if (data.weather[0].main == "Wind") {
+      background.style.backgroundImage = "url(img/wind_icon.png)"
+      background.style.backgroundColor = '#9B9B9B'
+    } else if (data.weather[0].main == "Fog") {
+      background.style.backgroundImage = "url(img/fog_icon.png)"
+      background.style.backgroundColor = '#BABABA'
     } else {
-      backgroundImage.style.backgroundImage = "url(img/sun.png)"
-    }
+      background.style.backgroundColor = '#4A90E2'    }
   }
   getImage()
   
 }
 
 getWeather()
-
-
-
 
